6ae8fcfb260b9403e2fe320d9e01da8ae9b3dcac
Evgeny Ovodkov <kefir4ek@gmail.com>
2020-09-29 20:54:31 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -92,4 +92,52 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	setClock('.timer', deadline);
+
+	// Modal
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+				modal = document.querySelector('.modal'),
+				modalCloseBtn = document.querySelector('[data-close]'),
+				modalTimerID = setTimeout(openModal, 20000);
+
+	function openModal() {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerID);
+	}
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
+
+	modalCloseBtn.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', (evt) => {
+		if (evt.target === modal) {
+			closeModal();
+		}
+	});
+
+	document.addEventListener('keydown', (evt) => {
+		if (evt.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	function showModalByScroll () {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
+
 });
