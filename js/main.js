1bd49808333a0fca0481fa7aa4cbe7d7aea2e58a
Evgeny Ovodkov <kefir4ek@gmail.com>
2020-10-10 20:41:54 +0300

    fetch api for forms

--- js/main.js
+++ js/main.js
@@ -231,30 +231,47 @@ window.addEventListener('DOMContentLoaded', () => {
 			`;
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-			request.setRequestHeader('Content-type', 'application/json');
-
 			const formData = new FormData(form);
 
 			const obj = {};
-			formData.forEach((value, key) => {
+			formData.forEach(function(value, key) {
 				obj[key] = value;
 			});
 
-			const json = JSON.stringify(obj);
-			request.send(json);
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success);
-					form.reset();
-					statusMessage.remove();
-				} else {
-					showThanksModal(message.failure);
-				}
+			fetch('server.php', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(obj)
+			})
+			.then(data => data.text())
+			.then(data => {
+				console.log(data);
+				showThanksModal(message.success);
+				form.reset();
+				statusMessage.remove();
+			})
+			.catch(() => {
+				showThanksModal(message.failure);
+			})
+			.finally(() => {
+				form.reset();
 			});
+
+			// const json = JSON.stringify(obj);
+			// request.send(json);
+
+			// request.addEventListener('load', () => {
+			// 	if (request.status === 200) {
+			// 		console.log(request.response);
+			// 		showThanksModal(message.success);
+			// 		form.reset();
+			// 		statusMessage.remove();
+			// 	} else {
+			// 		showThanksModal(message.failure);
+			// 	}
+			// });
 		});
 	}
 
